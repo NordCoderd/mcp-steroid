df6eda5c9d09a3a97649bfa1b1cb2a4ac796a15a
Eugene Petrenko <me@jonnyzzz.com>
2025-12-11 12:58:19 +0100

    Update gradle project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.1.0"
-    id("org.jetbrains.intellij.platform") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.10.5"
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.serialization") version "2.2.21"
 }
 
 group = "com.jonnyzzz.intellij"
@@ -8,6 +9,7 @@ version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
     intellijPlatform {
         defaultRepositories()
     }
@@ -15,9 +17,19 @@ repositories {
 
 dependencies {
     intellijPlatform {
-        intellijIdeaCommunity(providers.gradleProperty("platformVersion"))
-        instrumentationTools()
+        intellijIdeaUltimate("2025.3")
+        bundledPlugin("com.intellij.mcpServer")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
     }
+
+//    implementation("org.jetbrains.kotlinx:kotlinx-serialization-bom:1.9.0")
+//    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json")
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.10.2")
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines")
+//    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
 }
 
 kotlin {
@@ -28,5 +40,20 @@ intellijPlatform {
     pluginConfiguration {
         name = "IntelliJ MCP Steroid"
         version = project.version.toString()
+
+        ideaVersion {
+            sinceBuild = "252.1"
+            untilBuild = "*"
+        }
+    }
+}
+
+tasks {
+    test {
+        useJUnit()
+    }
+
+    processTestResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
     }
 }
