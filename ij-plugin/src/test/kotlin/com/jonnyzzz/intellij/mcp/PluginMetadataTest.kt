8721633a59b47683ee0f6a174a23546872a77181
Eugene Petrenko <me@jonnyzzz.com>
2026-01-31 18:09:52 +0100

    add tests for version check + cleanup distributions

---
+++ ij-plugin/src/test/kotlin/com/jonnyzzz/intellij/mcp/PluginMetadataTest.kt
@@ -0,0 +1,21 @@
+/* Copyright 2025-2026 Eugene Petrenko (mcp@jonnyzzz.com); Copyright 2025-2026 JetBrains. Use of this source code is governed by the Apache 2.0 license. */
+package com.jonnyzzz.intellij.mcp
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+class PluginMetadataTest : BasePlatformTestCase() {
+    fun testVersion() {
+        val version = getPluginVersion()
+        println("Version: $version")
+    }
+
+    fun testExpirationDate() {
+        val exp = getTimeBombExpiration()
+
+        println("Expiration Date: $exp")
+    }
+
+    fun testNotExpired() {
+        validateTimeBomb()
+    }
+}
