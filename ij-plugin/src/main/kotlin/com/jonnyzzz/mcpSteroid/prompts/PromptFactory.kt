dc9df8a4a34956490afe53ad8d831ef024bd4c33
Eugene Petrenko <me@jonnyzzz.com>
2026-02-05 02:05:31 +0100

    resources rework leftover, update PromptFactory

---
+++ ij-plugin/src/main/kotlin/com/jonnyzzz/mcpSteroid/prompts/PromptFactory.kt
@@ -0,0 +1,20 @@
+/* Copyright 2025-2026 Eugene Petrenko (mcp@jonnyzzz.com); Copyright 2025-2026 JetBrains. Use of this source code is governed by the Apache 2.0 license. */
+package com.jonnyzzz.mcpSteroid.prompts
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+
+@Service(Service.Level.APP)
+class PromptFactory {
+    inline fun <reified T:PromptBase> renderPrompt() = renderPrompt(T::class.java.constructors.single().newInstance() as T)
+
+    fun renderPrompt(prompt: PromptBase): String {
+        return prompt.readPrompt()
+    }
+}
+
+inline val promptFactory get() = service<PromptFactory>()
+
+sealed class PromptBase {
+    abstract fun readPrompt(): String
+}
