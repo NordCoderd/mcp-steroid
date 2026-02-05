22d5ce9f38559791d979a3a354f4f971888ad90a
Eugene Petrenko <me@jonnyzzz.com>
2026-02-05 21:06:42 +0100

    Remove empty comment placeholders from lsp-examples/find-references.kts

--- ij-plugin/src/main/prompts/lsp-examples/find-references.kts
+++ ij-plugin/src/main/prompts/lsp-examples/find-references.kts
@@ -1,20 +1,3 @@
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiManager
