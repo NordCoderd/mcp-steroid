dbbe21f57f0f038bac4c1a2bde1baf8f937c2096
Eugene Petrenko <me@jonnyzzz.com>
2026-02-05 21:06:42 +0100

    Remove empty comment placeholders from lsp-examples/go-to-definition.kts

--- ij-plugin/src/main/prompts/lsp-examples/go-to-definition.kts
+++ ij-plugin/src/main/prompts/lsp-examples/go-to-definition.kts
@@ -1,22 +1,3 @@
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.psi.PsiDocumentManager
