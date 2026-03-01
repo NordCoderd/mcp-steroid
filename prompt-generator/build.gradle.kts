74968b05fc68982e3832677d4cba3de756b04d75
Eugene Petrenko <me@jonnyzzz.com>
2026-03-01 21:40:32 +0100

    enable tests

--- prompt-generator/build.gradle.kts
+++ prompt-generator/build.gradle.kts
@@ -17,4 +17,12 @@ application {
 
 dependencies {
     implementation("com.squareup:kotlinpoet:2.2.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
